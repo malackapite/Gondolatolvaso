ad683be6157f84c8dcf19950eaa6f11e5d4765b3
nguyen.d.duy <nguyen.d.duy@FUJI112.szaleziakk.local>
2023-09-11 13:27:36 +0200

    második megoldás bekérés ellenörzés

--- src/gondolatolvaso/Jatekter.java
+++ src/gondolatolvaso/Jatekter.java
@@ -11,6 +11,7 @@ public class Jatekter {
         for(int ix=0; ix<kartyak.length;ix++)
             this.kartyak[ix]=ix+1;
         Kirak();
+        Melyik();
     }
     
     
@@ -19,8 +20,13 @@ public class Jatekter {
            System.out.println("%d\t%d\t%d".formatted(kartyak[ix],kartyak[ix+1],kartyak[ix+2]));
     }
     
-    private int Melyik(){
-        return sc.nextInt();
+    private void Melyik() {
+        boolean jo;
+        do {
+        System.out.print("melyik oszlop (1-3): ");
+        int bekertSzam = sc.nextInt();
+        jo = bekertSzam >= 1 && bekertSzam <= 3;
+        } while (!jo);
     }
     
     private void Kever(){
